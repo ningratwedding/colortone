53bb9ea25f22079389c1e0dc1411dae4c5b02f63
Your Name <ningratwedding@gmail.com>
2025-11-07 07:39:20 +0000

    logo header sepertinya terlalu menempel pada tepi halaman

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -32,7 +32,7 @@ const navLinks = [
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
+      <div className="container flex h-16 items-center px-4 md:px-6">
         <div className="md:hidden mr-4">
             <Sheet>
             <SheetTrigger asChild>
