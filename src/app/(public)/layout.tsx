6de4eca56895c08ac405112d6c5ae5e8699047c2
ningrat wedding <ningratwedding@gmail.com>
2025-11-07 11:30:30 +0000

    tambahkan copyriht di bawah

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { SiteFooter } from '@/components/site-footer';
 import { SiteHeader } from '@/components/site-header';
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
@@ -7,6 +8,7 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
     <>
       <SiteHeader />
       <main className="flex-grow">{children}</main>
+      <SiteFooter />
     </>
   );
 }
