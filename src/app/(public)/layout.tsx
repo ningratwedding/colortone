01a2f4fbafaf1033f97aff8c5ffaf75bbd9bfe86
ningrat wedding <ningratwedding@gmail.com>
2025-11-07 11:29:32 +0000

    hapus footer

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import { SiteHeader } from '@/components/site-header';
-import { SiteFooter } from '@/components/site-footer';
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SiteHeader />
       <main className="flex-grow">{children}</main>
-      <SiteFooter />
     </>
   );
 }
