60440b2ec6716d32228ba45c81d6c85392c630df
ningrat wedding <ningratwedding@gmail.com>
2025-11-08 05:25:48 +0000

    rata tengah dan responsif (_for element p_)

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 export function SiteFooter() {
     return (
         <footer className="border-t">
-            <div className="container flex h-16 items-center justify-center">
-                <p className="text-center text-sm leading-loose text-muted-foreground">
+            <div className="container flex h-16 items-center">
+                <p className="text-center text-sm leading-loose text-muted-foreground w-full">
                     &copy; {new Date().getFullYear()} FilterForge. Semua hak dilindungi undang-undang.
                 </p>
             </div>
