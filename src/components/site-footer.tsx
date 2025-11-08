92c57c1da6dca80d8733f9e7f0d87ff0c49132db
ningrat wedding <ningratwedding@gmail.com>
2025-11-08 02:09:11 +0000

    buat informasi hak cipta di dalam footer menjadi rata tengah

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -6,12 +6,10 @@ import Link from "next/link";
 export function SiteFooter() {
     return (
         <footer className="border-t">
-            <div className="container flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-                <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-                    <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                        &copy; {new Date().getFullYear()} FilterForge. Semua hak dilindungi undang-undang.
-                    </p>
-                </div>
+            <div className="container flex h-24 items-center justify-center">
+                <p className="text-center text-sm leading-loose text-muted-foreground">
+                    &copy; {new Date().getFullYear()} FilterForge. Semua hak dilindungi undang-undang.
+                </p>
             </div>
         </footer>
     );
