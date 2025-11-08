80344c729e98eb65ea699af9fdb395d969f3f5c2
ningrat wedding <ningratwedding@gmail.com>
2025-11-08 03:35:30 +0000

    ganti /creator/user-1 menjadi halaman /Profile/id

--- src/app/creator/settings/page.tsx
+++ src/app/creator/settings/page.tsx
@@ -43,7 +43,7 @@ function TikTokIcon(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
-      viewBox="0 0 24 24"
+      viewBox="0 0 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
