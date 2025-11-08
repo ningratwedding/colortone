e24b4c280f3375a69a07b46bc8ec805f106cdab5
ningrat wedding <ningratwedding@gmail.com>
2025-11-08 07:29:52 +0000

    saya ingin kompresi nukran dari 8k menjadi 2k aja misalnya

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,8 @@ const nextConfig: NextConfig = {
   },
   images: {
     quality: 65,
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
         protocol: 'https',
