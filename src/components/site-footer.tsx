37e094cb08d4e63c1b439a0528a41a174750cf42
ningrat wedding <ningratwedding@gmail.com>
2025-11-12 09:39:20 +0000

    max wide rata tengah (_for element div_)

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export function SiteFooter() {
     return (
         <footer className="border-t">
-            <div className="container flex flex-col sm:flex-row h-auto sm:h-16 items-center justify-between py-4 sm:py-0 gap-4">
+            <div className="mx-auto w-full max-w-none flex h-16 items-center justify-center px-4">
                  <p className="text-sm text-muted-foreground">
                     &copy; {new Date().getFullYear()} Di. Semua hak dilindungi undang-undang.
                 </p>
@@ -14,3 +14,4 @@ export function SiteFooter() {
         </footer>
     );
 }
+
