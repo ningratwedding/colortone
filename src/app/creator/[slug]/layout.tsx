1559346a31a2e27f90f62eccc5096ffd8ed51d9f
ningrat wedding <ningratwedding@gmail.com>
2025-11-12 08:00:12 +0000

    sharusnya halaman tersebut tidak berada di dalam directory halaman /crea

--- src/app/creator/[slug]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function CreatorProfileLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
-}
--- src/app/kreator/[slug]/page.tsx
+++ src/app/kreator/[slug]/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
@@ -185,10 +184,10 @@ function CreatorProfileContent({ slug }: { slug: string }) {
   );
 }
 
-export default function CreatorProfilePage({ params: { slug } }: { params: { slug: string } }) {
+export default function CreatorProfilePage({ params }: { params: { slug: string } }) {
     return (
         <>
-            <CreatorProfileContent slug={slug} />
+            <CreatorProfileContent slug={params.slug} />
             <SiteFooter />
         </>
     )
