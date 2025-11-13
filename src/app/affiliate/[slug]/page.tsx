cee40ff6db9510c22c0ed773dd001c5a2e93ccdf
ningrat wedding <ningratwedding@gmail.com>
2025-11-13 08:17:50 +0000

    sembunyikan header utama dari halaman /affiliate/affiliator

--- src/app/affiliate/[slug]/page.tsx
+++ src/app/affiliate/[slug]/page.tsx
@@ -1,13 +1,11 @@
 import { SiteFooter } from '@/components/site-footer';
 import * as React from 'react';
 import { AffiliateProfileContent } from './affiliate-profile-client';
-import { SiteHeader } from '@/components/site-header';
 
 export default function AffiliateProfilePage({ params }: { params: { slug: string } }) {
     
     return (
         <>
-            <SiteHeader />
             <AffiliateProfileContent slug={params.slug} />
             <SiteFooter />
         </>
