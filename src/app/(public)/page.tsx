370ce6ca6fe783bdfafb36c466d87bc38879fd31
ningrat wedding <ningratwedding@gmail.com>
2025-11-13 09:30:59 +0000

    hapus (_for element header_)

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -124,15 +124,6 @@ export default function Home() {
 
   return (
     <div className="container mx-auto px-4 py-6">
-      <header className="text-center mb-6">
-        <h1 className="text-3xl md:text-4xl font-bold tracking-tight text-primary font-headline">
-          Melek Digital
-        </h1>
-        <p className="mt-3 text-base text-muted-foreground max-w-2xl mx-auto">
-          Misi kami adalah menjadi platform terdepan yang memberdayakan para kreator. Kami menyediakan ruang di mana Anda dapat berbagi karya, menginspirasi, dan mengubah gairah menjadi peluang, baik produk digital maupun fisik.
-        </p>
-      </header>
-
       <div className="mb-4 flex flex-col md:flex-row gap-2 justify-end">
         <div className="flex gap-2 w-full md:w-auto">
            <Select
