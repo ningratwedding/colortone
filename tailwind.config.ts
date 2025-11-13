d31b0ea27282887d79f0cee0d3e8a0b11137e80d
ningrat wedding <ningratwedding@gmail.com>
2025-11-13 13:15:38 +0000

    kurangi ukuran font saat berada di dalam mode seluler

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,15 +10,15 @@ export default {
   theme: {
     extend: {
       fontSize: {
-        'xs': '.7rem',
-        'sm': '.8rem',
-        'base': '.9rem',
-        'lg': '1.05rem',
-        'xl': '1.15rem',
-        '2xl': '1.4rem',
-        '3xl': '1.75rem',
-        '4xl': '2.2rem',
-        '5xl': '2.8rem',
+        'xs': '.75rem',
+        'sm': '0.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
       },
       fontFamily: {
         body: ['Inter', 'sans-serif'],
