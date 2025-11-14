52f77ea21e9445852bd2a0bd8a8e7de547e968e6
ningrat wedding <ningratwedding@gmail.com>
2025-11-14 15:30:42 +0000

    sembunyikan header halaman Home

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,12 +1,16 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import { SiteFooter } from '@/components/site-footer';
 import { SiteHeader } from '@/components/site-header';
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isHomePage = pathname === '/';
+
   return (
     <>
-      <SiteHeader />
+      {!isHomePage && <SiteHeader />}
       <main className="flex-grow">{children}</main>
       <SiteFooter />
     </>
