611099dfc3a146741cdb8c5726c729b883e4b0a8
ningrat wedding <ningratwedding@gmail.com>
2025-11-14 04:27:57 +0000

    kenapa masih menggunakan localhost untuk nama domain ?

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,7 @@
 export const siteConfig = {
     name: "Di",
     description: "Temukan potensi kreatif Anda. Di adalah marketplace tempat para kreator menemukan, berbagi, dan menjual produk digital dan fisik yang unik.",
-    url: "http://localhost:9002", // TODO: Ganti dengan URL produksi Anda
-    ogImage: "https://picsum.photos/seed/og-image/1200/630"
+    url: "https://di.co",
+    ogImage: "https://picsum.photos/seed/og-image/1200/630",
+    supportEmail: "support@di.co"
 }
