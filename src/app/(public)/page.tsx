b59ab52c9efea386a68b2d4d85382ebd2af01c03
ningrat wedding <ningratwedding@gmail.com>
2025-11-18 02:41:52 +0000

    LinkStore.my.id (_for element span_)

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -124,11 +124,11 @@ export default function Home() {
           <div className="mt-8 mx-auto max-w-lg flex flex-col sm:flex-row items-center justify-center gap-2">
             <form onSubmit={handleClaimUsername} className="w-full sm:w-auto sm:flex-grow">
                 <div className="relative">
-                <span className="absolute left-4 top-1/2 -translate-y-1/2 text-muted-foreground font-medium">tokokita.id/</span>
+                <span className="absolute left-4 top-1/2 -translate-y-1/2 text-muted-foreground font-medium">LinkStore.my.id/</span>
                 <Input
                     type="text"
                     placeholder={placeholder}
-                    className="h-12 w-full rounded-full bg-background/90 text-foreground pl-[110px] sm:pl-[120px] pr-[100px] text-base"
+                    className="h-12 w-full rounded-full bg-background/90 text-foreground pl-[140px] pr-[100px] text-base"
                     value={slug}
                     onChange={(e) => setSlug(e.target.value)}
                     aria-label="Klaim nama toko Anda"
