7126c24437c26a07fbd007a3538148be0b7246ad
ningrat wedding <ningratwedding@gmail.com>
2025-11-18 03:35:43 +0000

    bagamaina sitemap dan robot ts ?

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -18,6 +18,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: `${baseUrl}/sellers`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
